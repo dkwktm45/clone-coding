ebccf48d033961983f506351fa9d744a334a7d5f
jin young lee <dkwktm45@naver.com>
2023-04-11 19:52:22 +0900

    게시글 저장 구현-1

--- src/main/java/com/hodoleg/clonecoding/controller/PostController.java
+++ src/main/java/com/hodoleg/clonecoding/controller/PostController.java
@@ -1,36 +1,24 @@
 package com.hodoleg.clonecoding.controller;
 
 import com.hodoleg.clonecoding.request.PostCreate;
+import com.hodoleg.clonecoding.service.PostService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 
-@Slf4j
+@Slf4j @RequiredArgsConstructor
 @RestController
 public class PostController {
 
-    // 웹 페이지 만드는 큰 방식
-    // SSR -> jsp , thymeleaf, mustache , freemarker
-    // html 랜더링 방식
+    private final PostService postService;
 
-    // SPA -> vue , nuxt ,next react
-    // javascript + < - > api(json) 방식
-
-    // 데이터를 검증하는 이유
-
-    // 1. client 개발자가 깜박할 수 있다. 실수로 값을 안보낼 수 있다.
-    // 2. client bug로 값이 누락될 수 있다.
-    // 3. 외부에 나쁜 사람이 값을 임의로 조작해서 보낼 수 있다.
-    // 4. DB에 값을 저장할 때 의도치 않은 오류가 발생할 수 있다.
-    // 5. 서버 개발자의 편안함을 위해서
     @PostMapping("/posts")
-    public Map<String, String> get(@RequestBody @Valid PostCreate params) {
-
-
-
+    public Map<String, String> get(@RequestBody @Valid PostCreate request) {
+        postService.write(request);
         return Map.of();
     }
 }
---
+++ src/main/java/com/hodoleg/clonecoding/service/PostService.java
@@ -0,0 +1,22 @@
+package com.hodoleg.clonecoding.service;
+
+import com.hodoleg.clonecoding.domain.Post;
+import com.hodoleg.clonecoding.request.PostCreate;
+import com.hodoleg.clonecoding.respository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+
+    public void write(PostCreate postCreate){
+        Post post = new Post(postCreate.getTitle(),postCreate.getContent());
+        postRepository.save(post);
+    }
+}
